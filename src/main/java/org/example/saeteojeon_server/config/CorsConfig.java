ad8f8565c783c294c1ba927122c648ec6d11768d
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-08-17 23:23:34 +0900

    cors설정 추가

---
+++ src/main/java/org/example/saeteojeon_server/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.example.saeteojeon_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://3.37.102.94:8080")
+                .allowedOrigins("http://3.37.102.94:8081")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+}
